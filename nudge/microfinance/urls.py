00d777ce12e4d78220737254dce1217a5eb69891
Santosh Kumar <san00718@gmail.com>
2015-08-16 06:36:53 +0530

    add list template

--- nudge/microfinance/urls.py
+++ nudge/microfinance/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^/sponsor(?P<sponsor_id>[0-9]+)/$',
         views.sponsor, name='get-sponsor-info'),
-    url(r'^/students/$',
+    url(r'^students/$',
         views.studentListing, name="students-list"),
 ]
