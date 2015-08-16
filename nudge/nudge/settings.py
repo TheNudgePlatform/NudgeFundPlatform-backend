72b098dba9791d4cd86d5f9da40e0a6999457bf6
Santosh Kumar <san00718@gmail.com>
2015-08-16 07:58:12 +0530

    fix imagefield storagewq

--- nudge/nudge/settings.py
+++ nudge/nudge/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = (
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.facebook',
+    #'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.linkedin',
     'django.contrib.admin',
     'django.contrib.auth',
